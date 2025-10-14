749531ae08679719ffeeccd876bf15ee5a5ec458
Ivan Buljovčić <ivan.buljovcic@gmail.com>
2025-10-14 13:13:01 +0200

    Feat/biome (#3)
    * chore: migrate to Biome for formatting and linting, update configurations
    
    * feat: biome check staged
    
    * feat: husky added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,26 @@
+import { clsx } from "clsx";
 import { createStrictClassSelector } from "@/lib/class-selectors";
 import utilitiesCss from "@/styles/utilities.module.css";
-import { clsx } from "clsx";
 import styles from "./page.module.css";
 
 const css = createStrictClassSelector(styles);
 const cssUtils = createStrictClassSelector(utilitiesCss);
 
 export default function Home() {
-	return <div className={clsx(css("container"), cssUtils("fixed", `block`))}>Hello world</div>;
+	return (
+		<div className={clsx(css("container"), cssUtils("fixed", `block`))}>
+			<div className={css("temp-block")} style={{ backgroundColor: "var(--color-base)" }}>
+				Base
+			</div>
+			<div className={css("temp-block")} style={{ backgroundColor: "var(--color-base-lighter)" }}>
+				Lighter
+			</div>
+			<div className={css("temp-block")} style={{ backgroundColor: "var(--color-base-darker)" }}>
+				Darker
+			</div>
+			<div className={css("temp-block")} style={{ backgroundColor: "var(--color-base-complement)" }}>
+				Complement
+			</div>
+		</div>
+	);
 }
